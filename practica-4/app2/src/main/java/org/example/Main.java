4df06ae28b910192791c80c058fccef14258145b
DemianReynoso <108692302+DemianReynoso@users.noreply.github.com>
2024-03-04 10:40:01 -0400

    Cambiando puerto en Main.java

--- practica-4/app2/src/main/java/org/example/Main.java
+++ practica-4/app2/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
 
-        Javalin app = Javalin.create().start(7000);
+        Javalin app = Javalin.create().start(7070);
 
         app.get("/", ctx -> {
             ctx.result("Bienvenido a mi app2!");
